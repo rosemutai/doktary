98e3ec93fd4a09fe952c91a7e2d9c9f178fc9aae
rosemutai <chepngetichrose2030@gmail.com>
2022-05-22 14:28:26 +0300

    custom tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      skew: {
+        '45': '50deg'
+      },
+
+      height: {
+          '128': '28rem',
+          
+      },
+
+      borderWidth: {
+        '1': '1px'
+      },
+
+      margin: {
+        '50': '20%'
+      }
+    },
+  },
+  plugins: [],
+}
