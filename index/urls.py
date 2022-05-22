3757a74a29e9f99fbdf0282d22f885a3883d0e3f
rosemutai <chepngetichrose2030@gmail.com>
2022-05-22 16:02:44 +0300

    urls

--- index/urls.py
+++ index/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import contact_view, book_appointment, index
+from .views import contact_view, book_appointment
 
 urlpatterns = [
-    path('', index, name='index'),
+    # path('', index, name='index'),
     path('contact/', contact_view, name="contact"),
     path('book-appointment/', book_appointment, name="book-appointment")
 ]
