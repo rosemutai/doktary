eecf1499fc6fb6781d3cb644b2b071b28bd49125
rosemutai <chepngetichrose2030@gmail.com>
2022-07-23 22:09:54 +0300

    postgres db settings

--- onaDoktary/settings.py
+++ onaDoktary/settings.py
@@ -33,6 +33,7 @@ DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['0.0.0.0', 'doktary.herokuapp.com', '127.0.0.1', 'localhost']
 
+
 CORS_ORIGIN_WHITELIST = [
     'https://localhost:3000',
 ]
@@ -99,12 +100,25 @@ WSGI_APPLICATION = 'onaDoktary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': str(os.getenv('DB_ENGINE')),
+        'NAME':  str(os.getenv('DB_NAME')),
+        'USER': str(os.getenv('DB_USER')),
+        'PASSWORD': str(os.getenv('DB_PASSWORD')),
+        'HOST': str(os.getenv('DB_HOST')),
+        'PORT': str(os.getenv('DB_PORT')),
     }
 }
+
+
 
 # DATABASES = {}
 # DATABASES['default'] = dj_database_url.config(conn_max_age=600)
