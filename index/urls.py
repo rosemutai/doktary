cac4c3c3f34387a47b096f9b4bd21910fd692412
rosemutai <chepngetichrose2030@gmail.com>
2022-07-26 18:55:57 +0300

    heroku settings

--- index/urls.py
+++ index/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import contact_view, book_appointment, index
+from .views import contact_view, book_appointment
+#  index
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
-    path('', index, name='index'),
+    # path('', index, name='index'),
     path('contact/', contact_view, name="contact"),
     path('book-appointment/', book_appointment, name="book-appointment")
 ]
--- index/views.py
+++ index/views.py
@@ -4,8 +4,8 @@ from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse, HttpResponseNotFound
 import os
 from django.views import View
-from django.views.generic import TemplateView
-from django.views.decorators.cache import never_cache
+# from django.views.generic import TemplateView
+# from django.views.decorators.cache import never_cache
 
 
 from rest_framework.decorators import api_view
@@ -17,7 +17,7 @@ from .serializers import ContactSerializer, BookAppointmentSerializer
 # Create your views here.
 
 # Serve Single Page Application
-index = never_cache(TemplateView.as_view(template_name='index.html'))
+# index = never_cache(TemplateView.as_view(template_name='index.html'))
 
 @api_view(['POST', ])
 def contact_view(request):
